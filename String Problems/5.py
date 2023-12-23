68ac58a734ced84b7873536acec9948f57253e42
RanaAk <102756910+RanaAk@users.noreply.github.com>
2023-12-23 05:54:11 +0000

    changes

---
+++ String Problems/5.py
@@ -0,0 +1,31 @@
+
+# # # def findL(s):
+# # #     a = s.split()
+# # #     for i in a:
+# # #         if(len(i)%2==0):
+# # #             print(len(i) )
+# # #         else:
+# # #             print("Odd")
+
+
+# # # print(findL("This is the lenght of the string"))
+
+
+
+# # #another type:
+
+
+# # n="his is the lenght of the string"
+
+# # s=n.split(" ") 
+# # l=list(filter(lambda x: (len(x)%2==0),s)) 
+# # print(l)
+
+# # #another type:
+
+# # python code to print even length words
+
+# n="This is the lenght of the string"
+# s=n.split(" ")
+# print([x for x in s if len(x)%2==0])
+
