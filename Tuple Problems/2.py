d3b8c39a3b5d65001409865446babe6491192673
RanaAk <102756910+RanaAk@users.noreply.github.com>
2023-12-24 05:04:50 +0000

    added

--- Tuple Problems/2.py
+++ Tuple Problems/2.py
@@ -1,20 +1,20 @@
 
-# test_tup = (1,2,3,4,5,6,7,8,9,10)
+test_tup = (1,2,3,4,5,6,7,8,9,10)
 
-# # printing original tuple
-# print("The original tuple is : " + str(test_tup))
+# printing original tuple
+print("The original tuple is : " + str(test_tup))
 
-# # printing result 
-
-
-# def max_min_tup(tup,k):
-#     tup = list(tup)
-#     temp =sorted(tup)
-#     res = (temp[:k] + temp [-k:])
-#     return res
+# printing result 
 
 
-# res = tuple(max_min_tup(test_tup,2))
-# print("The extracted values : " + str(res)) 
+def max_min_tup(tup,k):
+    tup = list(tup)
+    temp =sorted(tup)
+    res = (temp[:k] + temp [-k:])
+    return res
 
-# ####this is tup to list and then list to tup method
+
+res = tuple(max_min_tup(test_tup,2))
+print("The extracted values : " + str(res)) 
+
+####this is tup to list and then list to tup method
