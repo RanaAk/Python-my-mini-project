9c90c37797dc903fc6c0791dbf36f1d20ff849ec
RanaAk <102756910+RanaAk@users.noreply.github.com>
2023-12-24 05:04:22 +0000

    changes

--- Tuple Problems/1.py
+++ Tuple Problems/1.py
@@ -1,14 +1,14 @@
-# import sys
+import sys
 
-# Tuple1 = ("A", 1, "B", 2, "C", 3)
-# Tuple2 = ("i", "rana", "am", "ok")
-# Tuple3 = ((1,"a"), (2,"b"), (3,"c"), (4,"d"))
+Tuple1 = ("A", 1, "B", 2, "C", 3)
+Tuple2 = ("i", "rana", "am", "ok")
+Tuple3 = ((1,"a"), (2,"b"), (3,"c"), (4,"d"))
 
-# print("Size of Tuple1: " + str(sys.getsizeof(Tuple1)) + "bytes")
-# print("Size of Tuple2: " + str(sys.getsizeof(Tuple2)) + "bytes")
-# print("Size of Tuple3: " + str(sys.getsizeof(Tuple3)) + "bytes")
+print("Size of Tuple1: " + str(sys.getsizeof(Tuple1)) + "bytes")
+print("Size of Tuple2: " + str(sys.getsizeof(Tuple2)) + "bytes")
+print("Size of Tuple3: " + str(sys.getsizeof(Tuple3)) + "bytes")
 
-# print("another length type:, or we can say it ignores grabage values")
+print("another length type:, or we can say it ignores grabage values")
 
-# print(str("Size of Tuple1: " + str(len(Tuple1))))
-# print("Size of Tuple1: " + str(Tuple1.__sizeof__()))
+print(str("Size of Tuple1: " + str(len(Tuple1))))
+print("Size of Tuple1: " + str(Tuple1.__sizeof__()))
