2e045a4dc78a5699fd638e07c4d29a017d82e323
RanaAk <102756910+RanaAk@users.noreply.github.com>
2023-12-27 08:09:36 +0000

    new

--- Tuple Problems/3.py
+++ Tuple Problems/3.py
@@ -16,3 +16,18 @@
 # res = sum(list(map(sum, list(tup))))
 
 # print("The summation of tuple elements are : " + str(res))
+
+#######With math function librabry
+# import math
+# tup = ([1,2], [3,4], [5,6])
+# print("The tuple ;" + str(tup))
+
+
+# result = math.fsum(list(map(sum, list(tup))))
+# print(result)
+# # ###############but if the tup i lik #tup = (1,2,3,4,5,6)
+
+# tup = (1,2,3,4,5,6)
+
+# result = math.fsum(tup)
+# print(result)
---
+++ Tuple Problems/cube list tuple.py
@@ -0,0 +1,35 @@
+
+# # # list1 = [1, 2, 5, 6]
+# # # #with out cube
+
+# # # res = [(x,x) for x in list1]
+
+
+# # # print(res)
+
+# # # ###after Cube
+
+# # # result = [(x, pow(x,3)) for x in list1]
+# # # print("Cube of list is : ", result)
+
+# # # ####or 
+
+# # # result = [(x, x**3) for x in list1]
+# # # print("Cube of list is(Another method) : ",result)
+
+# # ####another type
+# # list1 = [1, 2, 5, 6]
+
+# # result = list(map(lambda x:(x, x**3), list1))
+# # print(result)
+
+# #one Beautiful method
+
+# list1 = [1, 2, 5, 6]
+# list2 = []
+
+# for x in list1:
+#     temp = (x, pow(x,3))
+#     list2.append(temp)
+
+# print(list2)
