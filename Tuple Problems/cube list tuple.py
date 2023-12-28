07c384eb9f76913755a2239623be3c207fa91fa2
RanaAk <102756910+RanaAk@users.noreply.github.com>
2023-12-28 18:54:20 +0000

    new

--- Tuple Problems/cube list tuple.py
+++ Tuple Problems/cube list tuple.py
@@ -19,19 +19,3 @@ print("Cube of list is(Another method) : ",result)
 
 ###################
 
-# ####another type
-list1 = [1, 2, 5, 6]
-
-result = list(map(lambda x:(x, x**3), list1))
-print(result)
-
-#### one Beautiful method
-
-list1 = [1, 2, 5, 6]
-list2 = []
-
-for x in list1:
-    temp = (x, pow(x,3))
-    list2.append(temp)
-
-print(list2)
