725af853d658a09339674930a7b286c47e95d5c7
RanaAk <102756910+RanaAk@users.noreply.github.com>
2023-12-30 06:22:08 +0000

    new

--- Tuple Problems/4.py
+++ Tuple Problems/4.py
@@ -9,29 +9,3 @@
 # print(result)
 # ############Np method
 
-import numpy as np
-
-test_tup = (1, 2, 3, 4, 5, 6)
-
- 
-#tup to np array
-test_array = np.array(test_tup)
- 
-
-print("The original tuple is : " + str(test_tup))
- 
-
-res = np.sum(test_array)
- 
-
-print("The summation of tuple elements are : " + str(res))
-
-####the fun part is np works on both
-tup = ([1,2], [3,4], [5,6])
-
-test = np.array(tup)
-
-result = np.sum(test)
-print("original tuple is : " + str(tup))
-
-print(result)
