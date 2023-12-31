253844142056baf6663bf9445d1aa51f99cff5cd
RanaAk <102756910+RanaAk@users.noreply.github.com>
2023-12-31 08:09:26 +0000

    another

--- Tuple Problems/3.py
+++ Tuple Problems/3.py
@@ -1,33 +1,33 @@
-# # def sumALl(tup):
-# # 	temp = list(tup)
-# # 	count = 0
-# # 	for i in temp:
-# # 		count += i
-# # 	return count
+# def sumALl(tup):
+# 	temp = list(tup)
+# 	count = 0
+# 	for i in temp:
+# 		count += i
+# 	return count
 
-# # tup = (5, 20, 3, 7, 6, 8)
-# # print(sumAll(tup))
-# #This method doesn't work on tup = ([1,2], [3,4], [5,6])
-# #SO here is another method
+# tup = (5, 20, 3, 7, 6, 8)
+# print(sumAll(tup))
+#This method doesn't work on tup = ([1,2], [3,4], [5,6])
+#SO here is another method
 
-# tup = ([1,2], [3,4], [5,6])
-# print("The tuple ;" + str(tup))
+tup = ([1,2], [3,4], [5,6])
+print("The tuple ;" + str(tup))
 
-# res = sum(list(map(sum, list(tup))))
+res = sum(list(map(sum, list(tup))))
 
-# print("The summation of tuple elements are : " + str(res))
+print("The summation of tuple elements are : " + str(res))
 
-#######With math function librabry
-# import math
-# tup = ([1,2], [3,4], [5,6])
-# print("The tuple ;" + str(tup))
+######With math function librabry
+import math
+tup = ([1,2], [3,4], [5,6])
+print("The tuple ;" + str(tup))
 
 
-# result = math.fsum(list(map(sum, list(tup))))
-# print(result)
-# # ###############but if the tup i lik #tup = (1,2,3,4,5,6)
+result = math.fsum(list(map(sum, list(tup))))
+print(result)
+# ###############but if the tup i lik #tup = (1,2,3,4,5,6)
 
-# tup = (1,2,3,4,5,6)
+tup = (1,2,3,4,5,6)
 
-# result = math.fsum(tup)
-# print(result)
+result = math.fsum(tup)
+print(result)
