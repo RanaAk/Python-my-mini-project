253f42c19299818c4e6c65d372e6d4d4accf0e6a
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-01 18:38:50 +0000

    new file added

--- Tuple Problems/3.py
+++ Tuple Problems/3.py
@@ -1,12 +1,12 @@
-# def sumALl(tup):
-# 	temp = list(tup)
-# 	count = 0
-# 	for i in temp:
-# 		count += i
-# 	return count
+def sumALl(tup):
+	temp = list(tup)
+	count = 0
+	for i in temp:
+		count += i
+	return count
 
-# tup = (5, 20, 3, 7, 6, 8)
-# print(sumAll(tup))
+tup = (5, 20, 3, 7, 6, 8)
+print(sumAll(tup))
 #This method doesn't work on tup = ([1,2], [3,4], [5,6])
 #SO here is another method
 
