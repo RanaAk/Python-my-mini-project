12cccbf084ada27557fc5bdac0c86ba546295e8c
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-02 07:55:59 +0000

    made changes

--- String Problems/5.py
+++ String Problems/5.py
@@ -12,20 +12,5 @@
 
 
 
-# #another type:
 
-
-n="his is the lenght of the string"
-
-s=n.split(" ") 
-l=list(filter(lambda x: (len(x)%2==0),s)) 
-print(l)
-
-# #another type:
-
-# python code to print even length words
-
-n="This is the lenght of the string"
-s=n.split(" ")
-print([x for x in s if len(x)%2==0])
 
