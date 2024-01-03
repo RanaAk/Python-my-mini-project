cc0347c0e655574ec17e63a25beaa7fca2d0710c
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-03 06:59:19 +0000

    little changes

--- String Problems/5.py
+++ String Problems/5.py
@@ -1,14 +1,14 @@
 
-# # def findL(s):
-# #     a = s.split()
-# #     for i in a:
-# #         if(len(i)%2==0):
-# #             print(len(i) )
-# #         else:
-# #             print("Odd")
+# def findL(s):
+#     a = s.split()
+#     for i in a:
+#         if(len(i)%2==0):
+#             print(len(i) )
+#         else:
+#             print("Odd")
 
 
-# # print(findL("This is the lenght of the string"))
+# print(findL("This is the lenght of the string"))
 
 
 
