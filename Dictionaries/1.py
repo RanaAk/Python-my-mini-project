4b7d0f09bba48f05c164850a587e20bd5a6ff83d
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-04 07:54:17 +0000

    added new files

---
+++ Dictionaries/1.py
@@ -0,0 +1,13 @@
+
+# my_dict = { 'a':'apple', 'b':'ball', 'c':'cat', 'd':'dog'}
+
+# inlist = list(my_dict.keys())
+# inlistvalues = list(my_dict.values())
+
+# inlist.sort()
+# inlistvalues.sort()
+
+# mydict = {i: my_dict[i] for i in inlist}
+
+# print(my_dict)
+# print(my_dict2)
---
+++ Dictionaries/2.py
@@ -0,0 +1,8 @@
+
+# from collections import OrderedDict
+
+# my_dict = { 'b' : '18', 'a' : '2', 'c' : '32', 'd' : '4'}
+
+# mydict = OrderedDict(sorted(my_dict.items()))
+
+# print(mydict)
