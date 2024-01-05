8612cf82287b8dfa695153e29db64fb3fbb475a3
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-05 17:24:01 +0000

    again

--- Dictionaries/5(2).py
+++ Dictionaries/5(2).py
@@ -1,17 +1,17 @@
 
-# data = {
-#     (5, "Sarah", "Williams"): {"a": "programming", "b": "development", "c": 80000},
-#     (6, "Michael", "Brown"): {"e": 25, "f": "coding", "g": 95000},
-#     (7, "Emily", "Davis"): {"h": 30, "i": "project", "j": "programming"},
-#     (8, "Daniel", "Wilson"): {"k": 35, "l": "testing", "m": 120000}
-# }
+data = {
+    (5, "Sarah", "Williams"): {"a": "programming", "b": "development", "c": 80000},
+    (6, "Michael", "Brown"): {"e": 25, "f": "coding", "g": 95000},
+    (7, "Emily", "Davis"): {"h": 30, "i": "project", "j": "programming"},
+    (8, "Daniel", "Wilson"): {"k": 35, "l": "testing", "m": 120000}
+}
 
-# # Accessing the values using the keys
-# print(data[(5, "Sarah", "Williams")]["a"])
-# print(data[(6, "Michael", "Brown")]["f"])
-# print(data[(7, "Emily", "Davis")]["j"])
+# Accessing the values using the keys
+print(data[(5, "Sarah", "Williams")]["a"])
+print(data[(6, "Michael", "Brown")]["f"])
+print(data[(7, "Emily", "Davis")]["j"])
 
-# data[(5, "Sarah", "Williams")]["a"] = {"b": "testing", "c": 80000}
-# data[(7, "Emily", "Davis")]["j"] = {"h": 30, "i": "project"}
-# print(data[(5, "Sarah", "Williams")]["a"])
-# print(data[(7, "Emily", "Davis")]["j"])
+data[(5, "Sarah", "Williams")]["a"] = {"b": "testing", "c": 80000}
+data[(7, "Emily", "Davis")]["j"] = {"h": 30, "i": "project"}
+print(data[(5, "Sarah", "Williams")]["a"])
+print(data[(7, "Emily", "Davis")]["j"])
