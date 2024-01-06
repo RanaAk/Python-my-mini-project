abc024779fad7e110d69fc3bc292d2cfe5fb5ca2
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-06 06:27:06 +0000

    another

--- Dictionaries/sum.py
+++ Dictionaries/sum.py
@@ -1,8 +1,21 @@
+# # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+
+# # j=0;
+# # for i in my_dict:
+# #     j+=my_dict[i]
+# #     print(j)
+
 # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
 
-# j=0;
-# for i in my_dict:
-#     j+=my_dict[i]
-#     print(j)
+# my_list = list(my_dict.values())
 
-my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+
+# print(sum(my_list))
+
+# #####or 
+# my_list = []
+# for i in my_dict:
+#     my_list.append(my_dict[i])
+
+# print(sum(my_list))
+
