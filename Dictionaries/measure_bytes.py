aa3a01094ac99b4d549ad4e58af8aa1c79997df6
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-07 07:51:39 +0000

    added

---
+++ Dictionaries/measure_bytes.py
@@ -0,0 +1,18 @@
+# # import sys
+
+# # # Measure bytes of an object
+# # def measure_bytes(obj):
+# #     print(str(sys.getsizeof(obj)) +  "bytes")
+
+
+# # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+# # measure_bytes(my_dict)
+
+# ######another type
+
+# def measure_bytes(obj):
+#     print(str(obj.__sizeof__()) + "bytes")
+#     print(obj.__sizeof__() , "bytes")
+
+# my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+# measure_bytes(my_dict)
--- Dictionaries/sum.py
+++ Dictionaries/sum.py
@@ -1,37 +1,64 @@
+# # # # # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+
+# # # # # j=0;
+# # # # # for i in my_dict:
+# # # # #     j+=my_dict[i]
+# # # # #     print(j)
+
+# # # # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+
+# # # # my_list = list(my_dict.values())
+
+
+# # # # print(sum(my_list))
+
+# # # # #####or 
+# # # # my_list = []
+# # # # for i in my_dict:
+# # # #     my_list.append(my_dict[i])
+
+# # # # print(sum(my_list))
+
+# # # ####or we can return with direct valus
+
+# # # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+
+# # # print(sum(my_dict.values()))
+# # # ###we can do this in function
+
+# # # def sum_dict_values(my_dict):
+# # #     return sum(my_dict.values())
+
+
+# # # def main():
+# # #     sum_dict_values(my_dict)
+
+# # # if __name__ == "__main__":
+# # #     main()
+
+# # ####another beautiful function example
 # # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+# # def sum_dict_values(my_dict):
+# #     return sum(my_dict[key] for key in my_dict)
 
-# # j=0;
-# # for i in my_dict:
-# #     j+=my_dict[i]
-# #     print(j)
+# # def main():
+# #     a = sum_dict_values(my_dict)
+# #     print(a)
 
+# # if __name__ == "__main__":
+# #     main()
+
+# ######with np function
+
+# import numpy as np
 # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
 
-# my_list = list(my_dict.values())
+# def sum_dict_values(my_dict):
+#     my_list = list(my_dict.values())
+#     my_array = np.array(my_list)
+#     my_sum = np.sum(my_array)
+#     return my_sum
 
 
-# print(sum(my_list))
+# print(sum_dict_values(my_dict))
 
-# #####or 
-# my_list = []
-# for i in my_dict:
-#     my_list.append(my_dict[i])
-
-# print(sum(my_list))
-
-####or we can return with direct valus
-
-my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
-
-print(sum(my_dict.values()))
-###we can do this in function
-
-def sum_dict_values(my_dict):
-    return sum(my_dict.values())
-
-
-def main():
-    sum_dict_values(my_dict)
-
-if __name__ == "__main__":
-    main()
