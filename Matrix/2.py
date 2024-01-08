c772842f0e0e6f1919f15920adb2c2a526d1921f
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-08 07:10:41 +0000

    Another one

--- Matrix/2.py
+++ Matrix/2.py
@@ -1,10 +1,7 @@
 
-# printing result
-print("The Assigned Matrix : " + str(res))
 
+list_ = [[1, 2, 3], [4, 5, 6], [7, 8, 9],[10,11,12]]
 
-list_ = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+result = dict(zip(list_[0], list_[1:]))
 
-reesult = dict(zip(list_[0], list_[1:]))
-
-print(reesult)
+print(result)
