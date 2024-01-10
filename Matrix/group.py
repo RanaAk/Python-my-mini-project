c59e1049e15d05497c228bf7c3f7df7bbaa0e2f5
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-10 16:30:53 +0000

    add

--- Matrix/group.py
+++ Matrix/group.py
@@ -1,31 +1,31 @@
 
 
-# # my_list = [1,2,1,4,5,1,5,6,6,7,8]
-
-# # List = []
-
-# # for i in my_list:
-# #     found = False
-# #     for j in range(len(List)):
-# #         if i in List[j]:
-# #             List[j].append(i)
-# #             found = True
-# #             break
-# #     if not found:
-# #         List.append([i])
-    
-# # print(str(List))
-
-# ######Another type of solution
-
 # my_list = [1,2,1,4,5,1,5,6,6,7,8]
 
-# group = {}
+# List = []
 
 # for i in my_list:
-#     if i in group:
-#         group[i].append(i)
-#     else:
-#         group[i] = [i]
+#     found = False
+#     for j in range(len(List)):
+#         if i in List[j]:
+#             List[j].append(i)
+#             found = True
+#             break
+#     if not found:
+#         List.append([i])
+    
+# print(str(List))
 
-# print(list(group.values()))
+######Another type of solution
+
+my_list = [1,2,1,4,5,1,5,6,6,7,8]
+
+group = {}
+
+for i in my_list:
+    if i in group:
+        group[i].append(i)
+    else:
+        group[i] = [i]
+
+print(list(group.values()))
