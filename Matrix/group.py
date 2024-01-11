8144d0114e02202e325cc899345264cd3d6ea7fe
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-11 18:14:28 +0000

    add

--- Matrix/group.py
+++ Matrix/group.py
@@ -1,18 +1,18 @@
 
 
-# my_list = [1,2,1,4,5,1,5,6,6,7,8]
+my_list = [1,2,1,4,5,1,5,6,6,7,8]
 
-# List = []
+List = []
 
-# for i in my_list:
-#     found = False
-#     for j in range(len(List)):
-#         if i in List[j]:
-#             List[j].append(i)
-#             found = True
-#             break
-#     if not found:
-#         List.append([i])
+for i in my_list:
+    found = False
+    for j in range(len(List)):
+        if i in List[j]:
+            List[j].append(i)
+            found = True
+            break
+    if not found:
+        List.append([i])
     
-# print(str(List))
+print(str(List))
 
