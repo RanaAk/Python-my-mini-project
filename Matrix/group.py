62ca977e619237d4265f7db49d99e284bf10dc7e
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-11 18:13:38 +0000

    little changes

--- Matrix/group.py
+++ Matrix/group.py
@@ -16,16 +16,3 @@
     
 # print(str(List))
 
-######Another type of solution
-
-my_list = [1,2,1,4,5,1,5,6,6,7,8]
-
-group = {}
-
-for i in my_list:
-    if i in group:
-        group[i].append(i)
-    else:
-        group[i] = [i]
-
-print(list(group.values()))
