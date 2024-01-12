5a6ea99ab88f0286800569acd492766cf92a860e
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-12 15:05:02 +0000

    new file added

--- Matrix/2.py
+++ Matrix/2.py
@@ -1,7 +1,7 @@
 
 
-# list_ = [[1, 2, 3], [4, 5, 6], [7, 8, 9],[10,11,12]]
+list_ = [[1, 2, 3], [4, 5, 6], [7, 8, 9],[10,11,12]]
 
-# result = dict(zip(list_[0], list_[1:]))
+result = dict(zip(list_[0], list_[1:]))
 
-# print(result)
+print(result)
