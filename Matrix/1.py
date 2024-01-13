b1b0443254ff11a712215d3be1ab0c1a2956f5a9
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-13 06:55:51 +0000

    some changes

--- Matrix/1.py
+++ Matrix/1.py
@@ -10,15 +10,5 @@
 
 # print(update_list)
 
-### Another type of solution
 
-update = {}
-
-for i in range(len(test_list)-1):
-    update[test_list[0][i]] = test_list[i+1]
-
-print(update)
-
-
-# my_matrix = ()
 
