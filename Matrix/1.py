13a1b28dbf8afc0e85cf02aebc0a942ba9013b4f
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-13 06:56:30 +0000

    Update

--- Matrix/1.py
+++ Matrix/1.py
@@ -1,14 +1,14 @@
 
 
-# test_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+test_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-# print(str(test_list))
-# print(test_list[0])
+print(str(test_list))
+print(test_list[0])
 
-# update_list  = {test_list[0][i] :test_list[i+1] for i in range(len(test_list)-1) }
+update_list  = {test_list[0][i] :test_list[i+1] for i in range(len(test_list)-1) }
 
 
-# print(update_list)
+print(update_list)
 
 
 
