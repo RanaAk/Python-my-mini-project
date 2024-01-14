0dbc398fae5d171c18c81bf6ad8eadd5c49dbd9c
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-14 06:20:38 +0000

    update

--- Matrix/add/add.py
+++ Matrix/add/add.py
@@ -19,24 +19,24 @@
 # # print("This is matrix sub\n",matrix_sub([[1,2],[3,4]],[[1,2],[3,4]]))
 # # ############but this method is not work for different length of matrix
 
-# ###thats why we are goint to use zip function
+###thats why we are goint to use zip function
 
-# def matrix_add(A,B):
-#     result = []
-#     for row_a,row_b in zip(A,B):
-#         result.append([sum(x) for x in zip(row_a,row_b)])
-#     return result
+def matrix_add(A,B):
+    result = []
+    for row_a,row_b in zip(A,B):
+        result.append([sum(x) for x in zip(row_a,row_b)])
+    return result
 
-# def matrix_sub(A,B):
-#     result = []
-#     for row_a,row_b in zip(A,B):
-#         result.append([x-y for x,y in zip(row_a,row_b)])
-#     return result
+def matrix_sub(A,B):
+    result = []
+    for row_a,row_b in zip(A,B):
+        result.append([x-y for x,y in zip(row_a,row_b)])
+    return result
 
-# print("This is matrix add\n",matrix_add([[1,2],[3,4]],[[1,2],[3,4]]))
-# print("This is matrix sub\n",matrix_sub([[1,2],[3,4]],[[1,2],[3,4]]))
+print("This is matrix add\n",matrix_add([[1,2],[3,4]],[[1,2],[3,4]]))
+print("This is matrix sub\n",matrix_sub([[1,2],[3,4]],[[1,2],[3,4]]))
 
-#####this time we are going to use ower own method 
+####this time we are going to use ower own method 
 
 A = [[1,2],[3,4]]
 B = [[1,2],[3,4]]
