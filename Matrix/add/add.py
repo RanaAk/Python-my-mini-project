dd7fe0002c84f5463364b0870f9125ac8a1e3bfd
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-14 06:18:55 +0000

    added

--- Matrix/add/add.py
+++ Matrix/add/add.py
@@ -1,52 +1,52 @@
 
-# # # import numpy as np
-# # # def matrix_add(A,B):
-
-# # #     A = np.array(A)
-# # #     B = np.array(B)
-
-# # #     return np.add(A,B)
-
-# # # def matrix_sub(A,B):
-
-# # #     A = np.array(A)
-# # #     B = np.array(B)
-
-# # #     return np.subtract(A,B)
-
-
-# # # print("This is matrix add"+ str(matrix_add([[1,2],[3,4]],[[1,2],[3,4]])))
-# # # print("This is matrix sub\n",matrix_sub([[1,2],[3,4]],[[1,2],[3,4]]))
-# # # ############but this method is not work for different length of matrix
-
-# # ###thats why we are goint to use zip function
-
+# # import numpy as np
 # # def matrix_add(A,B):
-# #     result = []
-# #     for row_a,row_b in zip(A,B):
-# #         result.append([sum(x) for x in zip(row_a,row_b)])
-# #     return result
+
+# #     A = np.array(A)
+# #     B = np.array(B)
+
+# #     return np.add(A,B)
 
 # # def matrix_sub(A,B):
-# #     result = []
-# #     for row_a,row_b in zip(A,B):
-# #         result.append([x-y for x,y in zip(row_a,row_b)])
-# #     return result
 
-# # print("This is matrix add\n",matrix_add([[1,2],[3,4]],[[1,2],[3,4]]))
+# #     A = np.array(A)
+# #     B = np.array(B)
+
+# #     return np.subtract(A,B)
+
+
+# # print("This is matrix add"+ str(matrix_add([[1,2],[3,4]],[[1,2],[3,4]])))
 # # print("This is matrix sub\n",matrix_sub([[1,2],[3,4]],[[1,2],[3,4]]))
+# # ############but this method is not work for different length of matrix
 
-# #####this time we are going to use ower own method 
+# ###thats why we are goint to use zip function
 
-# A = [[1,2],[3,4]]
-# B = [[1,2],[3,4]]
+# def matrix_add(A,B):
+#     result = []
+#     for row_a,row_b in zip(A,B):
+#         result.append([sum(x) for x in zip(row_a,row_b)])
+#     return result
 
-# result = [[0,0],[0,0]]
-# for i in range(len(A)):
-#     for j in range(len(A[0])):
-#         result[i][j] = A[i][j] + B[i][j]
+# def matrix_sub(A,B):
+#     result = []
+#     for row_a,row_b in zip(A,B):
+#         result.append([x-y for x,y in zip(row_a,row_b)])
+#     return result
 
-# for i in result:
-#     for j in  i:
-#         print(j,end=" ")
-#     print()
+# print("This is matrix add\n",matrix_add([[1,2],[3,4]],[[1,2],[3,4]]))
+# print("This is matrix sub\n",matrix_sub([[1,2],[3,4]],[[1,2],[3,4]]))
+
+#####this time we are going to use ower own method 
+
+A = [[1,2],[3,4]]
+B = [[1,2],[3,4]]
+
+result = [[0,0],[0,0]]
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        result[i][j] = A[i][j] + B[i][j]
+
+for i in result:
+    for j in  i:
+        print(j,end=" ")
+    print()
