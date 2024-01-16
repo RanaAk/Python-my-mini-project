a6679d7768f718918490fdaca57e1182474e051b
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-16 20:23:54 +0000

    new file

--- Matrix/add/add.py
+++ Matrix/add/add.py
@@ -1,22 +1,22 @@
 
-# # import numpy as np
-# # def matrix_add(A,B):
+import numpy as np
+def matrix_add(A,B):
 
-# #     A = np.array(A)
-# #     B = np.array(B)
+    A = np.array(A)
+    B = np.array(B)
 
-# #     return np.add(A,B)
+    return np.add(A,B)
 
-# # def matrix_sub(A,B):
+def matrix_sub(A,B):
 
-# #     A = np.array(A)
-# #     B = np.array(B)
+    A = np.array(A)
+    B = np.array(B)
 
-# #     return np.subtract(A,B)
+    return np.subtract(A,B)
 
 
-# # print("This is matrix add"+ str(matrix_add([[1,2],[3,4]],[[1,2],[3,4]])))
-# # print("This is matrix sub\n",matrix_sub([[1,2],[3,4]],[[1,2],[3,4]]))
-# # ############but this method is not work for different length of matrix
+print("This is matrix add"+ str(matrix_add([[1,2],[3,4]],[[1,2],[3,4]])))
+print("This is matrix sub\n",matrix_sub([[1,2],[3,4]],[[1,2],[3,4]]))
+############but this method is not work for different length of matrix
 
 
