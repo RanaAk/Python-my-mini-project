ea74f8b65e584245bd0a5628000732a30f645716
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-18 19:06:04 +0000

    neww add

--- Dictionaries/3.py
+++ Dictionaries/3.py
@@ -1,8 +1,8 @@
 
 
 
-# my_dict = { 'b' : 18, 'a' :2, 'c' : 32, 'd' : 4}
+my_dict = { 'b' : 18, 'a' :2, 'c' : 32, 'd' : 4}
 
-# mydict = sorted(my_dict.items(), key = lambda x: (x[1],x[0]))
+mydict = sorted(my_dict.items(), key = lambda x: (x[1],x[0]))
 
-# print(mydict)
+print(mydict)
