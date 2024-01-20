d7d48d6328a977b2cee106525652beffe49acd10
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-20 15:57:45 +0000

    update

--- Dictionaries/4.py
+++ Dictionaries/4.py
@@ -32,19 +32,3 @@
 # except KeyError:
 #     print("Not found")
 
-####Now in sort order
-
-my_dict = { 'b' : 18, 'a' :2, 'c' : 32, 'd' : 4}
-#my_dict = sorted(mydict.items(), key = lambda x: (x[1],x[0]))
-try:
-    print(my_dict)
-    print(list(my_dict))
-    my = list(my_dict.values())
-    my.sort()
-    print(my)
-    sorted_dict = {i: my_dict[i] for i in my}
-    print(sorted_dict)
-    print(" ")
-    print(my_dict['e'])
-except KeyError:
-    print("Not found")
