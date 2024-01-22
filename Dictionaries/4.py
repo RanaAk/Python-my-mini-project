402f285bf07dd0269063558a9cbbc09d4c04a161
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-22 18:19:56 +0000

    Remove unnecessary code and comments from Dictionaries/4.py

--- Dictionaries/4.py
+++ Dictionaries/4.py
@@ -10,25 +10,5 @@
 # else:
 #     print("Not found")
 
-######Another example. Even we can set default value
 
-my_dict = { 'b' : 18, 'a' :2, 'c' : 32, 'd' : 4}
-
-key = 'e'
-
-my_dict.setdefault(key, 'Not found')
-
-print(my_dict[key])
-###We can try and get
-
-my_dict = { 'b' : 18, 'a' :2, 'c' : 32, 'd' : 4}
-#my_dict = sorted(mydict.items(), key = lambda x: (x[1],x[0]))
-try:
-    print(my_dict)
-    print(list(my_dict))
-    print(list(my_dict.values()))
-    print(" ")
-    print(my_dict['e'])
-except KeyError:
-    print("Not found")
 
