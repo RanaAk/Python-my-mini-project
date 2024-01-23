ddaf78604a98bb59ac0c689cdf951910bb536294
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-23 17:44:54 +0000

    Fix dictionary key variable names and remove unnecessary code

--- Dictionaries/5.py
+++ Dictionaries/5.py
@@ -1,22 +1,22 @@
-# #we can print like that
+#we can print like that
 
-# my_dict = {("1'2", "4'5") : "ok", ("1'9", "4'8") : "not ok"}
-# #my_dict = {("19.07.53.2", "72.54'51.0"):"Mumbai", 
-# #             ("28.33'34.1", "77.06'16.6"):"Delhi"}
-# key = []
+my_dict = {("1'2", "4'5") : "ok", ("1'9", "4'8") : "not ok"}
+#my_dict = {("19.07.53.2", "72.54'51.0"):"Mumbai", 
+#             ("28.33'34.1", "77.06'16.6"):"Delhi"}
+key = []
 
-# key_2 = []
+key_2 = []
 
-# f = []
+f = []
 
-# for i in my_dict:
-#     key.append(i[0])
-#     key_2.append(i[1])
+for i in my_dict:
+    key.append(i[0])
+    key_2.append(i[1])
 
 
-# print(key)
-# print(key_2)   
-# print(f)
+print(key)
+print(key_2)   
+print(f)
 
 
 
