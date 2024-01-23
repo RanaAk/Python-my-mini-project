fd3b04387e13b0a3b145ee8a7c124374386035f0
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-23 17:44:11 +0000

    Fix dictionary access and print statement in 4.py

--- Dictionaries/4.py
+++ Dictionaries/4.py
@@ -1,14 +1,14 @@
-# # my_dict = { 'b' : 18, 'a' :2, 'c' : 32, 'd' : 4}
+# my_dict = { 'b' : 18, 'a' :2, 'c' : 32, 'd' : 4}
 
-# # val = 'a'
-# # print(my_dict.get(val, "Not found"))
+# val = 'a'
+# print(my_dict.get(val, "Not found"))
 
-# ######Another example
-# val = 'e'
-# if my_dict.get(val):
-#     print(my_dict[val])
-# else:
-#     print("Not found")
+######Another example
+val = 'e'
+if my_dict.get(val):
+    print(my_dict[val])
+else:
+    print("Not found")
 
 
 
