d8d1112ba4ea0f63d256dd419a7b5b5528207f36
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-26 10:15:05 +0000

    Refactor measure_bytes function to import sys and measure the bytes of an object

--- Dictionaries/measure_bytes.py
+++ Dictionaries/measure_bytes.py
@@ -1,11 +1,11 @@
-# import sys
+import sys
 
-# # Measure bytes of an object
-# def measure_bytes(obj):
-#     print(str(sys.getsizeof(obj)) +  "bytes")
+# Measure bytes of an object
+def measure_bytes(obj):
+    print(str(sys.getsizeof(obj)) +  "bytes")
 
 
-# my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
-# measure_bytes(my_dict)
+my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+measure_bytes(my_dict)
 
 
