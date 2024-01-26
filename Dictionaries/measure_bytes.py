c1472079e07329d2a73808435b2a0f3a5de18b28
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-26 10:14:46 +0000

    Remove unnecessary code in measure_bytes function

--- Dictionaries/measure_bytes.py
+++ Dictionaries/measure_bytes.py
@@ -8,11 +8,4 @@
 # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
 # measure_bytes(my_dict)
 
-######another type
 
-def measure_bytes(obj):
-    print(str(obj.__sizeof__()) + "bytes")
-    print(obj.__sizeof__() , "bytes")
-
-my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
-measure_bytes(my_dict)
