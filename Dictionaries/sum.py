88eefb1fb1809a1b0cc2144d34907b0d0b4f8070
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-30 06:56:54 +0000

    Refactor sum_dict_values function in Dictionaries/sum.py

--- Dictionaries/sum.py
+++ Dictionaries/sum.py
@@ -36,17 +36,17 @@
 # # if __name__ == "__main__":
 # #     main()
 
-# ####another beautiful function example
-# my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
-# def sum_dict_values(my_dict):
-#     return sum(my_dict[key] for key in my_dict)
+####another beautiful function example
+my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+def sum_dict_values(my_dict):
+    return sum(my_dict[key] for key in my_dict)
 
-# def main():
-#     a = sum_dict_values(my_dict)
-#     print(a)
+def main():
+    a = sum_dict_values(my_dict)
+    print(a)
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 ######with np function
 
