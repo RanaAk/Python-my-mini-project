d0f9b8ef868d5eb5740cfef2258545bb39295bbf
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-01-31 09:22:50 +0000

    Refactor sum_dict_values function to remove unnecessary numpy dependency

--- Dictionaries/sum.py
+++ Dictionaries/sum.py
@@ -48,17 +48,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-######with np function
-
-import numpy as np
-my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
-
-def sum_dict_values(my_dict):
-    my_list = list(my_dict.values())
-    my_array = np.array(my_list)
-    my_sum = np.sum(my_array)
-    return my_sum
-
-
-print(sum_dict_values(my_dict))
 
