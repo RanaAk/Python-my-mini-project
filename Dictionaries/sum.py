5aa014b24146a80a8d64e52f7559a91f9201d541
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-02-03 06:52:13 +0000

    Remove unused code and commented out function in sum.py

--- Dictionaries/sum.py
+++ Dictionaries/sum.py
@@ -36,16 +36,6 @@
 # # if __name__ == "__main__":
 # #     main()
 
-####another beautiful function example
-my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
-def sum_dict_values(my_dict):
-    return sum(my_dict[key] for key in my_dict)
 
-def main():
-    a = sum_dict_values(my_dict)
-    print(a)
-
-if __name__ == "__main__":
-    main()
 
 
