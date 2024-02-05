a2c95155edb7b0a18b1c4182a7d4976008e12b02
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-02-05 07:39:07 +0000

    Refactor sum_dict_values function and add main function

--- Dictionaries/sum.py
+++ Dictionaries/sum.py
@@ -1,40 +1,40 @@
-# # # # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
-
-# # # # j=0;
-# # # # for i in my_dict:
-# # # #     j+=my_dict[i]
-# # # #     print(j)
-
 # # # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
 
-# # # my_list = list(my_dict.values())
-
-
-# # # print(sum(my_list))
-
-# # # #####or 
-# # # my_list = []
+# # # j=0;
 # # # for i in my_dict:
-# # #     my_list.append(my_dict[i])
-
-# # # print(sum(my_list))
-
-# # ####or we can return with direct valus
+# # #     j+=my_dict[i]
+# # #     print(j)
 
 # # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
 
-# # print(sum(my_dict.values()))
-# # ###we can do this in function
-
-# # def sum_dict_values(my_dict):
-# #     return sum(my_dict.values())
+# # my_list = list(my_dict.values())
 
 
-# # def main():
-# #     sum_dict_values(my_dict)
+# # print(sum(my_list))
 
-# # if __name__ == "__main__":
-# #     main()
+# # #####or 
+# # my_list = []
+# # for i in my_dict:
+# #     my_list.append(my_dict[i])
+
+# # print(sum(my_list))
+
+####or we can return with direct valus
+
+my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+
+print(sum(my_dict.values()))
+###we can do this in function
+
+def sum_dict_values(my_dict):
+    return sum(my_dict.values())
+
+
+def main():
+    sum_dict_values(my_dict)
+
+if __name__ == "__main__":
+    main()
 
 
 
