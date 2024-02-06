ab9d6db63690d7e06216bffadcdeb52605fa5a81
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-02-06 07:45:29 +0000

    Refactor sum function in Dictionaries/sum.py

--- Dictionaries/sum.py
+++ Dictionaries/sum.py
@@ -1,23 +1,23 @@
-# # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
-
-# # j=0;
-# # for i in my_dict:
-# #     j+=my_dict[i]
-# #     print(j)
-
 # my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
 
-# my_list = list(my_dict.values())
-
-
-# print(sum(my_list))
-
-# #####or 
-# my_list = []
+# j=0;
 # for i in my_dict:
-#     my_list.append(my_dict[i])
+#     j+=my_dict[i]
+#     print(j)
 
-# print(sum(my_list))
+my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+
+my_list = list(my_dict.values())
+
+
+print(sum(my_list))
+
+#####or 
+my_list = []
+for i in my_dict:
+    my_list.append(my_dict[i])
+
+print(sum(my_list))
 
 
 
