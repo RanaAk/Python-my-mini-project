0733ee23122f9c28de5f739f52d1d75e6c0d2716
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-02-08 07:20:11 +0000

    Commented out unnecessary code in sum.py

--- Dictionaries/sum.py
+++ Dictionaries/sum.py
@@ -1,9 +1,9 @@
-my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
+# my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
 
-j=0;
-for i in my_dict:
-    j+=my_dict[i]
-    print(j)
+# j=0;
+# for i in my_dict:
+#     j+=my_dict[i]
+#     print(j)
 
 my_dict = { 'a': 1, 'b': 2, 'c': 3, 'd': 4 }
 
