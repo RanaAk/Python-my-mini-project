5bb8385d4a5431ca4d4d62e82248c1babaf4c1dd
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-02-14 07:18:21 +0000

    Refactor set removal logic

--- List Problems/Set/remove.py
+++ List Problems/Set/remove.py
@@ -6,6 +6,6 @@ for i in range(len(my_set)):
 
 my_set = set([12, 10, 13, 15, 8, 9])
 
-# while my_set:
-#     my_set.discard(max(my_set))
-#     print(my_set)
+while my_set:
+    my_set.discard(max(my_set))
+    print(my_set)
