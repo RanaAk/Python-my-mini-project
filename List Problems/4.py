3e51d889197a51ef683e959cbea740c7d3333d39
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-02-17 14:33:00 +0000

    update

--- List Problems/4.py
+++ List Problems/4.py
@@ -13,8 +13,8 @@
 #     list[pos1], list[pos2] = list[pos2], list[pos1]
 #     return list
 
-# a = [1,2,3,4,5,6,7]
-# print(swaplist(a,1,2))
+a = [1,2,3,4,5,6,7]
+print(swaplist(a,1,2))
 
 def swapList(list, pos1, pos2):
     get = list[pos1], list[pos2]
