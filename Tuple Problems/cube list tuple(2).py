deb94185b9882f251b0562764fa029d8180b82cb
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-03-13 11:16:06 +0000

    update

--- Tuple Problems/cube list tuple(2).py
+++ Tuple Problems/cube list tuple(2).py
@@ -1,16 +1,16 @@
-# ####another type
-list1 = [1, 2, 5, 6]
+# # ####another type
+# list1 = [1, 2, 5, 6]
 
-result = list(map(lambda x:(x, x**3), list1))
-print(result)
+# result = list(map(lambda x:(x, x**3), list1))
+# print(result)
 
-#### one Beautiful method
+# #### one Beautiful method
 
-list1 = [1, 2, 5, 6]
-list2 = []
+# list1 = [1, 2, 5, 6]
+# list2 = []
 
-for x in list1:
-    temp = (x, pow(x,3))
-    list2.append(temp)
+# for x in list1:
+#     temp = (x, pow(x,3))
+#     list2.append(temp)
 
-print(list2)
+# print(list2)
