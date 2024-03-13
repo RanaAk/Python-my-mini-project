8f86b7b1c6330ee464579c3d98e4c700fc05b864
RanaAk <102756910+RanaAk@users.noreply.github.com>
2024-03-13 11:16:35 +0000

    Up

--- Tuple Problems/4.py
+++ Tuple Problems/4.py
@@ -1,11 +1,11 @@
-#Combine Zip function
+# #Combine Zip function
 
-tup1= (1, 2, 3)
-tup2= (4, 5, 6)
-tup3= (7, 8, 9)
+# tup1= (1, 2, 3)
+# tup2= (4, 5, 6)
+# tup3= (7, 8, 9)
  
-com = zip(tup1, tup2, tup3)
-result = tuple(map(sum, comb))
-print(result)
-############Np method
+# com = zip(tup1, tup2, tup3)
+# result = tuple(map(sum, comb))
+# print(result)
+# ############Np method
 
